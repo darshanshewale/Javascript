f37c1583191f93972379e8cdcd8e39ab39b400b2
darshanshewale <60446982+darshanshewale@users.noreply.github.com>
2024-01-29 15:13:36 +0530

    JS basic

---
+++ external.js
@@ -0,0 +1,5 @@
+function exter()
+{
+  document.getElementById("demo2")
+  .innerHTML="External JS"
+}
---
+++ javascript.js
@@ -0,0 +1,41 @@
+// Javascript is the most popular lightwigth ,interpreted comipled programming language It can be used for both client-side as well as
+// server side developments Javasctript is also know as a scritping language for wabpages
+
+// Things that makes javascript demanding
+// @No need for compilers:since javascript is an interpreted language therefore it does not need any compliler for complilation
+// @used both client and server side :evoulution of its frameword namely node.js and XPathExpression.js it is now widely used to building server 
+// side application to
+// @help to build complete solution:java script is widely used in both client and server side application therefore it helps us to build an end to
+// end solution to agiven problem
+// @used everywhere : javascript is so loved beacuse it can be used everywhere it can be used to develop website ,games or mobile apps 
+// @huge commmunity support : many dvelopers in the community 
+
+
+// javascript is a lighwight ,cross-platform .single-threaded and interpreted compiled programming language. it is also kknown as the scripting
+// language for web pages it is well know for the development of web pages and many non-browser environments also use it
+
+// client side: It supplies objects to control a browser and its Document object model(DOM) Like if client-side extensions allow an application 
+// to place elements on an HTML form and responde to user event such as mouse click ,form input and page navigation useful libararies for the 
+// client side are angukar js ,reactjs ,vuejs and so many others
+
+// server side:It supplies objects relevant to running javascript on a sevrer for if the server-side extension allow an application to communicate
+// with the database and provide continuity of information from one invocation to another of the applicaiton or perform file manipulation on a server
+// the useful framework which is the most famous these days is Node.js
+
+// Imperative language : In this type of langauage we are mostly concerned about how it  it to be done 
+
+// Declarative programming L basically here logical computation  requires 
+
+
+// how to link java script file inn HTML 
+
+// Internal Js : We can add java script directly to our HTML file by writing the code inside the <script>tag the <script>tag can either 
+// be placed inside th <head> or the <boday> tag according to the requirement 
+
+// External Js : we can write javascript code is another files having an extension.js and then link this file inside the <head> tag 
+// of the HTML  file in which we want to add this code 
+
+  
+
+
+
